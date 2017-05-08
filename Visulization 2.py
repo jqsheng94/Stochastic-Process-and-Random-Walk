43541b5645d46fb361d029bab83b0fd25bb6f0fb
Jiaqi Sheng <jsheng@bbtv.com>
2017-05-08 14:27:25 -0700

    Visulization of movements

---
+++ Visulization 2.py
@@ -0,0 +1,58 @@
+
+import matplotlib.pyplot as plt
+import matplotlib.style
+from matplotlib import animation
+from statistics import mean
+import random
+
+
+class RandomWalk():
+    def __init__(self, **kwargs):
+        self.NumberOfSteps = kwargs.get("NumberOfSteps")
+        self.NumberOfInteration = kwargs.get("NumberOfInteration")
+        self.Left = 0.3
+        self.Right = 0.25
+        self.Up = 0.35
+        self.Down = 0.1
+        self.radius = 10
+
+    def OneIeteration(self):
+        matplotlib.style.use('ggplot')
+        fig, ax = plt.subplots()
+        ax.set_xlim(-12, 12)
+        ax.set_ylim(-12, 12)
+        circ = plt.Circle((0, 0), radius=10, fill=False, color='brown', linewidth=2)
+        ax.add_patch(circ)
+        Points = []
+        initial = (0, 0)
+        for i in range(self.NumberOfSteps):
+            if i == 0:
+                new_x = [initial[0]]
+                new_y = [initial[1]]
+                points, = ax.plot(new_x, new_y, marker='o')
+            else:
+                p = random.uniform(0, 1)
+                if p <= self.Left:
+                    initial = (initial[0] - 1, initial[1])
+                elif p > self.Left and p <= self.Left + self.Right:
+                    initial = (initial[0] + 1, initial[1])
+                elif p > self.Left + self.Right and p <= self.Left + self.Right + self.Up:
+                    initial = (initial[0], initial[1] + 1)
+                else:
+                    initial = (initial[0], initial[1] - 1)
+                new_x = [initial[0]]
+                new_y = [initial[1]]
+                points.set_data(new_x, new_y)
+                if (initial[0]) ** 2 + (initial[1]) ** 2 >= self.radius ** 2:
+                    break
+            Points.append((new_x, new_y))
+            plt.pause(0.5)
+        return len(Points)
+
+
+
+Animation = RandomWalk(NumberOfSteps = 10000, NumberOfInteration = 10000).OneIeteration()
+print(Animation)
+
+
+
---
+++ Visulization.py
@@ -0,0 +1,66 @@
+import numpy as np
+from statistics import mean
+import matplotlib.pyplot as plt
+import matplotlib.style
+import random
+from matplotlib import animation
+
+
+Left = 0.3
+Right = 0.25
+Up = 0.35
+Down = 0.1
+radius = 10
+initial = (0, 0)
+
+
+Points = []
+
+for i in range(10000):
+    if i == 0:
+        x = [initial[0]]
+        y = [initial[1]]
+    else:
+        p = random.uniform(0, 1)
+        if p <= Left:
+            initial = (initial[0] - 1, initial[1])
+        elif p > Left and p <= Left + Right:
+            initial = (initial[0] + 1, initial[1])
+        elif p > Left + Right and p <= Left + Right + Up:
+            initial = (initial[0], initial[1] + 1)
+        else:
+            initial = (initial[0], initial[1] - 1)
+        x = [initial[0]]
+        y = [initial[1]]
+        if (initial[0]) ** 2 + (initial[1]) ** 2 >= radius ** 2:
+            break
+    Points.append((x, y))
+
+def init():
+    line.set_data([], [])
+    return line,
+
+def animate(s):
+    A = Points[s]
+    xs = np.array(A[0])
+    ys = np.array(A[1])
+    line.set_data(xs, ys)
+    return line,
+
+
+matplotlib.style.use('ggplot')
+fig = plt.figure()
+ax = plt.axes(xlim=(-12, 12), ylim=(-12, 12))
+line, = ax.plot([], [], lw=2, marker='o')
+circ = plt.Circle((0, 0), radius=10, fill=False, color='brown', linewidth=2)
+ax.add_patch(circ)
+
+
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=len(Points), interval=500, blit=True)
+
+anim.save('RandomWalk.mp4',extra_args=['-vcodec', 'libx264'])
+
+plt.show()
+
+
